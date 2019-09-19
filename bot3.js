60b88f6d368bd310379a42a8a70a85e93aea3550
Anth0ny1 <anthonythi51@gmail.com>
2019-09-19 21:04:51 +0200

    test

--- bot3.js
+++ bot3.js
@@ -1,9 +1,12 @@
-const { Client, Util } = require('discord.js');
+// const { Client, Util } = require('discord.js');
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
 // const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 // const YouTube = require('simple-youtube-api');
 // const ytdl = require('ytdl-core');
 
-const client = new Client({ disableEveryone: true });
+// const client = new Client({ disableEveryone: true });
 
 const youtube = new YouTube(GOOGLE_API_KEY);
 
